46b5eaa36a62c31567a4535cb4d8d4507f62e933
meyhane <egemen.yavuz@soprasteria.com>
2021-02-05 14:53:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,200 @@
+# azure-ad
+[![Build Status](https://travis-ci.org/sesam-community/azure-ad.svg?branch=master)](https://travis-ci.org/sesam-community/azure-ad)
+
+Sesam - Microsoft Graph API connector that can serve both as a source or sink.
+Features:
+  * supports the whole Graph API as is
+  * offers dedicated URL routes for _user_, _group_ and _planner_ operations. See "URL paths" section below.
+  * implements cached authentication
+  * streams results for GET requests
+  * handles paging implicitly
+
+### Azure AD access
+
+To be able to communicate with Azure AD we need to register an applicaiton in Azure AD and obtain client id and client secret needed for Oauth:
+
+Navigate to https://aad.portal.azure.com -> Dashboard -> App Registrations and create new app
+
+You also need to assign required permissions or/and roles to this app such as `User.ReadWrite.All, Directory.ReadWrite.All, Group.ReadWrite.All`  
+read more [here](https://docs.microsoft.com/en-us/graph/api/user-post-users?view=graph-rest-1.0&tabs=cs) and [here](https://stackoverflow.com/questions/52626067/microsoft-graph-api-insufficient-privileges-when-trying-to-update-mobilephone)
+
+This app uses Outh2 authentication with either `client_credentials`, `code` or `password` grant type. You need to provide next environment variables:
+
+* `client_id` - from registered application
+* `client_secret` - from registered application
+* `tenant_id` - Tenant id my be found in Azure AD properties  
+
+in addition for `password` grant type  
+* `username` - registered user, couldn't be an invited user or user with 2 factor auth turned on
+* `password` - password for this user
+
+To be able to use  authorization code flow you need to navigate first to `/auth` endpoint and perform authentication as a user.
+All next requests will then use acquired token and refresh it if needed by using refresh_token.  
+
+**Remember to add correct `redirect_uri` to application properties in Azure.**
+
+By default `client_credentials` schema used for every request, to use `password` schema add query parameter `auth` with value `user` to request.
+
+You may control MS Graph API version with environmental variable `API_VERSION`
+
+
+By default all queries executed with `odata.metadata=minimal`. This behavior may be changed by using  environmental variable `ODATA_METADATA` (available values: `none`, `minimal`, `full`).
+
+
+### URL paths
+
+ * **/datasets/user/entities** : methods=['GET'], get users
+ * **/datasets/group/entities** : methods=['GET'], get groups
+ * **/datasets/<path:kind>/entities** : methods=['GET'], get objects of given type from MS graph API
+ * **/planner/plans/entities** , methods=['GET'], get all plans from Microsoft Planner service
+ * **/planner/tasks/entities** , methods=['GET'], get all tasks from Microsoft Planner service
+ * **/datasets/user** : methods=['POST'], create or update or delete(disable) users. Handles create/update/disable operation based on the precence of _id_ field, or 'resource exists' error, or \_deleted value
+ * **/datasets/group** : methods=['POST'], same as /datasets/user handler but for _group_ object
+ * **/graphapi/&lt;path:path&gt;** : methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], Generic route for MS graph API. Call it in the same way as you would call the graph API directly except that you don't need to think about authorization. Returns 200 for all GET requests even if error occurs, and for all successful calls. Returns 500 for erronous calls.
+ * **/auth** : methods=['GET'], signs in user interactively by using Microsoft login page
+
+### System setup
+
+```json
+{
+  "_id": "ms-graph-test-service",
+  "type": "system:microservice",
+  "docker": {
+    "environment": {
+      "LOG_LEVEL": "DEBUG|INFO|WARNING|ERROR",
+      "client_id": "<client id>",
+      "client_secret": "<client secret>",
+      "tenant_id": "<azure tenant id>"
+    },
+    "image": "<docker image>",
+    "port": 5000
+  },
+  "verify_ssl": true
+}
+```
+
+### Setup for "fetch" pipe to retrieve Azure AD users
+
+```json
+{
+  "_id": "<pipe id>",
+  "type": "pipe",
+  "source": {
+    "type": "json",
+    "system": "ms-graph-test-service",
+    "url": "/datasets/users/entities"
+  }
+}
+
+```
+
+### Setup for "fetch" pipe to retrieve another resources available through MS Graph API
+**Here used to fetch lists for given Sharepoint site**  
+*you will need `Sites.Read.All` permission to perform this request*
+
+```json
+{
+  "_id": "<pipe id>",
+  "type": "pipe",
+  "source": {
+    "type": "json",
+    "system": "ms-graph-test-service",
+    "url": "/datasets/sites/<sharepoint site id>/lists/entities"
+  }
+}
+
+```
+
+### Setup for "fetch" pipe to retrieve Microsoft Planner plans through MS Graph API
+*Some of groups may throw 403 exception which is logged but swallowed so pipe don't receive any error*
+*Check system log after fetching plans to be sure if everything is ok*
+
+```json
+{
+  "_id": "<pipe id>",
+  "type": "pipe",
+  "source": {
+    "type": "json",
+    "system": "ms-graph-test-service",
+    "url": "/planner/plans/entities"
+  }
+}
+
+```
+
+
+### Setup for "fetch" pipe to retrieve Microsoft Planner tasks through MS Graph API
+*Some of groups may throw 403 exception which is logged but swallowed so pipe don't receive any error*
+*Check system log after fetching plans to be sure if everything is ok*
+
+```json
+{
+  "_id": "<pipe id>",
+  "type": "pipe",
+  "source": {
+    "type": "json",
+    "system": "ms-graph-test-service",
+    "url": "/planner/tasks/entities"
+  }
+}
+
+```
+
+### Setup for "create/update" pipe
+
+You need to supply correct representation of user. There are different requirements when you create or update an user.  
+For example `passwordProfile` must be supplied to create user but not allowed when you update user.  
+Another very important attribute is `userPrincipalName` -  By convention, this should map to the user's email name. The general format is alias@domain, where domain must be present in the tenant’s collection of verified domains.   
+
+Read more [here](https://docs.microsoft.com/en-us/graph/api/resources/user?view=graph-rest-beta)
+
+```json
+{
+  "_id": "<pipe id>",
+  "type": "pipe",
+  "source": {
+    "type": "dataset",
+    "dataset": "<source dataset name>"
+  },
+  "sink": {
+    "type": "json",
+    "system": "<sink system id>",
+    "url": "/datasets/user"
+  },
+  "transform": {
+    "type": "dtl",
+    "rules": {
+      "default": [
+        ["copy", "*"],
+        ["add", "jobTitle", "programmer"]
+      ]
+    }
+  }
+}
+
+```
+
+```json
+{
+  "_id": "<pipe id>",
+  "type": "pipe",
+  "source": {
+    "type": "dataset",
+    "dataset": "<source dataset name>"
+  },
+  "sink": {
+    "type": "json",
+    "system": "<sink system id>",
+    "url": "/graphapi/applications"
+  },
+  "transform": {
+    "type": "dtl",
+    "rules": {
+      "default": [
+        ["add", "displayName", "Display name"]
+      ]
+    }
+  }
+}
+
+```
---
+++ service/service.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+
+import logging
+import datetime
+import os
+import json
+import uuid
+from flask import Flask, Response, request as r, redirect, session
+from requests.exceptions import HTTPError
+
+from auth_helper import get_authorize_url, get_token_with_auth_code, add_token_to_cache
+from plan_dao import get_plans, get_tasks
+from str_utils import str_to_bool
+from user_dao import sync_user_array, get_all_users
+from group_dao import sync_group_array, get_all_groups
+from dao_helper import init_dao, get_all_objects, init_dao_on_behalf_on, stream_as_json, make_request, GRAPH_URL, ALLOWED_METHODS
+from logger_helper import log_request
+
+env = os.environ.get
+
+APP = Flask(__name__)
+LOG_LEVEL = env('LOG_LEVEL', "INFO")
+PORT = int(env('PORT', '5000'))
+CT = 'application/json'
+
+SUPPORTS_SINCE = str_to_bool(env('SUPPORTS_SINCE', 'false'))
+
+# used to encrypt user sessions
+APP.secret_key = uuid.uuid4().bytes
+
+
+@APP.route('/datasets/user/entities', methods=['GET'])
+@log_request
+def list_users():
+    """
+    Endpoint to fetch all users from Azure AD via MS graph API
+    :request_argument since - delta token returned from last request (if exist)
+    :return: JSON array with fetched users
+    """
+    init_dao(env('client_id'), env('client_secret'), env('tenant_id'))
+    return Response(get_all_users(r.args.get('since')), content_type=CT)
+
+
+@APP.route('/datasets/group/entities', methods=['GET'])
+@log_request
+def list_groups():
+    """
+    Endpoint to fetch all groups from Azure AD via MS graph API
+    :request_argument since - delta token returned from last request (if exist)
+    :return: JSON array with fetched groups
+    """
+    init_dao(env('client_id'), env('client_secret'), env('tenant_id'))
+    return Response(get_all_groups(r.args.get('since')), content_type=CT)
+
+
+@APP.route('/datasets/<path:kind>/entities', methods=['GET'])
+@log_request
+def list_objects(kind):
+    """
+    Endpoint to fetch all objects of given type from MS graph API
+    :request_argument since - delta token returned from last request (if exist)
+    :return: JSON array with fetched groups
+    """
+    if r.args.get('auth') and r.args.get('auth') == 'user':
+        init_dao_on_behalf_on(env('client_id'), env('client_secret'), env('tenant_id'), env('username'),
+                              env('password'))
+    else:
+        init_dao(env('client_id'), env('client_secret'), env('tenant_id'))
+    return Response(
+        stream_as_json(get_all_objects(f'/{kind}/{"delta" if SUPPORTS_SINCE else ""}', r.args.get('since'))),
+        content_type=CT)
+
+
+@APP.route('/planner/plans/entities', methods=['GET'])
+@log_request
+def list_all_plans():
+    """
+    Endpoint to list all plans from Microsoft Planner service
+    :return:
+    """
+    if r.args.get('auth') and r.args.get('auth') == 'user':
+        init_dao_on_behalf_on(env('client_id'), env('client_secret'), env('tenant_id'), env('username'),
+                              env('password'))
+    else:
+        init_dao(env('client_id'), env('client_secret'), env('tenant_id'))
+    return Response(stream_as_json(get_plans(get_all_objects('/groups/'))), r.args.get('since'), content_type=CT)
+
+
+@APP.route('/planner/tasks/entities', methods=['GET'])
+@log_request
+def list_all_tasks():
+    if r.args.get('auth') and r.args.get('auth') == 'user':
+        init_dao_on_behalf_on(env('client_id'), env('client_secret'), env('tenant_id'), env('username'),
+                              env('password'))
+    else:
+        init_dao(env('client_id'), env('client_secret'), env('tenant_id'))
+    return Response(stream_as_json(get_tasks(get_plans(get_all_objects('/groups/')))), content_type=CT)
+
+
+@APP.route('/datasets/user', methods=['POST'])
+@log_request
+def post_users():
+    """
+    Endpoint to synchronize users from Sesam into Azure AD
+    :return: 200 empty response if everything OK
+    """
+    if r.args.get('auth') and r.args.get('auth') == 'user':
+        init_dao_on_behalf_on(env('client_id'), env('client_secret'), env('tenant_id'), env('username'),
+                              env('password'))
+    else:
+        init_dao(env('client_id'), env('client_secret'), env('tenant_id'))
+    sync_user_array(json.loads(r.data))
+    return Response('')
+
+
+@APP.route('/datasets/group', methods=['POST'])
+@log_request
+def post_groups():
+    """
+    Endpoint to synchronize groups from Sesam into Azure AD
+    :return: 200 empty response if everything OK
+    """
+    if r.args.get('auth') and r.args.get('auth') == 'user':
+        init_dao_on_behalf_on(env('client_id'), env('client_secret'), env('tenant_id'), env('username'),
+                              env('password'))
+    else:
+        init_dao(env('client_id'), env('client_secret'), env('tenant_id'))
+    sync_group_array(json.loads(r.data))
+    return Response('')
+
+@APP.route('/graphapi/<path:path>', methods=ALLOWED_METHODS)
+@log_request
+def generic_graph_api_request(path=None):
+    """
+    Generic Endpoint to call Microsoft Graph API.
+    Any request in Microsoft Graph API can be issued here.
+    :return: 200 for successful GET requests
+             200 and malformed json for erronous GET requests
+             code and response from graghapi for other cases
+    """
+    if r.args.get('auth') and r.args.get('auth') == 'user':
+        init_dao_on_behalf_on(env('client_id'), env('client_secret'), env('tenant_id'), env('username'),
+                              env('password'))
+    else:
+        init_dao(env('client_id'), env('client_secret'), env('tenant_id'))
+    try:
+        if r.method.lower() == 'get':
+            response = Response(stream_as_json(get_all_objects(f'/{path}')), content_type=CT, status=200)
+        else:
+            url=f'{GRAPH_URL}/{path}'
+            data=json.loads(r.data) if r.data else None
+
+            response = Response(json.dumps(make_request(url=url, method=r.method, data=data)), content_type=CT, status=200)
+    except HTTPError as error:
+        logging.exception(error)
+        response = Response(error.response.text, content_type=CT, status=error.response.status_code)
+
+    return response
+
+@APP.route('/auth', methods=['GET'])
+@log_request
+def auth_user():
+    """
+    Endpoint to sign in user interactively by using Microsoft login page
+    :return:
+    """
+    state = str(datetime.datetime.now().timestamp()) if 'state' not in session else session['state']
+    tenant = env('tenant_id')
+    client_id = env('client_id')
+    client_secret = env('client_secret')
+    redir_url = env('redirect_url')
+
+    if not r.args.get('code'):
+        session['state'] = state
+        return redirect(get_authorize_url(tenant, client_id, state, redir_url))
+    else:
+        code = r.args.get('code')
+        returned_state = r.args.get('state')
+        if state != returned_state:
+            print(state)
+            print(returned_state)
+            raise SystemError("Response state doesn't match request state")
+
+        token = get_token_with_auth_code(tenant, client_id, client_secret, code, redir_url)
+        if 'access_token' in token:
+            add_token_to_cache(client_id, tenant, token)
+            return Response(json.dumps({'status': 'ok', 'message': 'token acquired'}), content_type=CT)
+        else:
+            raise ValueError("token response malformed")
+
+
+if __name__ == '__main__':
+    """
+    Application entry point
+    """
+    logging.basicConfig(level=logging.getLevelName(LOG_LEVEL))
+
+    IS_DEBUG_ENABLED = True if logging.getLogger().isEnabledFor(logging.DEBUG) else False
+
+    if IS_DEBUG_ENABLED:
+        APP.run(debug=IS_DEBUG_ENABLED, host='0.0.0.0', port=PORT)
+    else:
+        import cherrypy
+
+        cherrypy.tree.graft(APP, '/')
+        cherrypy.config.update({
+            'environment': 'production',
+            'engine.autoreload_on': True,
+            'log.screen': False,
+            'server.socket_port': PORT,
+            'server.socket_host': '0.0.0.0',
+            'server.thread_pool': 10,
+            'server.max_request_body_size': 0
+        })
+
+        cherrypy.engine.start()
+        cherrypy.engine.block()
